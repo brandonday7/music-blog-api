b4800457f79e0e202d3bd2ecc9320253c29e7e9b
Brandon Day <brandontday7@gmail.com>
2018-10-23 23:22:23 -0400

    added better seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,14 @@ Artist.create!(name: "Talking Heads")
 Artist.create!(name: "The 1975")
 Artist.create!(name: "Frank Ocean")
 
-Post.create!(title: "Day 1", description: "Favourite song", image_ref: "https://www.rhino.com/sites/rhino.com/files/styles/square/public/aod/075992388320.jpg?itok=m1f27u9i")
-Post.create!(title: "Day 2", description: "Today's commute", image_ref: "https://cdn.albumoftheyear.org/album/remain-in-light.jpg")
+Post.create!(title: "Oldies I", description: "Iron and Wine cover is cool too", image_ref: "https://www.rhino.com/sites/rhino.com/files/styles/square/public/aod/075992388320.jpg?itok=m1f27u9i")
+Post.create!(title: "Oldies II", description: "Strangest music video I've ever seen", image_ref: "https://cdn.albumoftheyear.org/album/remain-in-light.jpg")
 
-Post.create!(title: "Day 3", description: "Feeling nostaligc", image_ref: "https://i1.sndcdn.com/artworks-000149663235-6fsjeg-t500x500.jpg")
-Post.create!(title: "Day 4", description: "Best new song", image_ref: "https://static.stereogum.com/uploads/2018/05/the-1975-give-yourself-a-try-1527789760-640x640.jpg")
+Post.create!(title: "Best Band in the World I", description: "Feeling nostaligc", image_ref: "https://i1.sndcdn.com/artworks-000149663235-6fsjeg-t500x500.jpg")
+Post.create!(title: "Best Band in the World II", description: "Best new song", image_ref: "https://static.stereogum.com/uploads/2018/05/the-1975-give-yourself-a-try-1527789760-640x640.jpg")
 
-Post.create!(title: "Day 5", description: "Great single", image_ref: "https://thefader-res.cloudinary.com/private_images/w_760,c_limit,f_auto,q_auto:best/single-artwork-for-frank-ocean-s-lens_xgecql/single-artwork-for-frank-ocean-s-lens.jpg")
-Post.create!(title: "Day 6", description: "Nice rhyming", image_ref: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7c/Frank_Ocean_Chanel_Cover.jpg/220px-Frank_Ocean_Chanel_Cover.jpg")
+Post.create!(title: "Favourite Frank Songs I", description: "Great piano", image_ref: "https://thefader-res.cloudinary.com/private_images/w_760,c_limit,f_auto,q_auto:best/single-artwork-for-frank-ocean-s-lens_xgecql/single-artwork-for-frank-ocean-s-lens.jpg")
+Post.create!(title: "Favourite Frank Songs II", description: "Nice rhyming", image_ref: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7c/Frank_Ocean_Chanel_Cover.jpg/220px-Frank_Ocean_Chanel_Cover.jpg")
 
 Track.create!(title: "This Must Be The Place", artist_id: Artist.all[0].id, post_id: Post.all[0].id)
 Track.create!(title: "Once in a Lifetime", artist_id: Artist.all[0].id, post_id: Post.all[1].id)
