4f9aadc36333aa929221ed88a857adf2646c65e6
Brandon Day <brandontday7@gmail.com>
2018-10-23 22:41:32 -0400

    better seed file

--- app/graphql/mutations/delete_post.rb
+++ app/graphql/mutations/delete_post.rb
@@ -8,20 +8,15 @@ class Mutations::DeletePost < GraphQL::Schema::RelayClassicMutation
   def resolve (id:)
     post = Post.find(id)
     if post 
-      puts "***"
-      puts "in here"
       track = post.track
       artist = track.artist
       track.delete
-      puts "deleted track"
 
       if artist.tracks.length == 1
         artist.delete
-        puts "deleted artist"
       end
 
       deleted = post.delete
-      puts "deleted post"
 
     end
     if deleted
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,23 +11,22 @@ Artist.create!(name: "Talking Heads")
 Artist.create!(name: "The 1975")
 Artist.create!(name: "Frank Ocean")
 
-Post.create!(title: "Day 1", description: "Favourite song")
-Post.create!(title: "Day 2", description: "Today's commute")
+Post.create!(title: "Day 1", description: "Favourite song", image_ref: "https://www.rhino.com/sites/rhino.com/files/styles/square/public/aod/075992388320.jpg?itok=m1f27u9i")
+Post.create!(title: "Day 2", description: "Today's commute", image_ref: "https://cdn.albumoftheyear.org/album/remain-in-light.jpg")
 
-Post.create!(title: "Day 3", description: "Feeling nostaligc")
-Post.create!(title: "Day 4", description: "Best new song")
+Post.create!(title: "Day 3", description: "Feeling nostaligc", image_ref: "https://i1.sndcdn.com/artworks-000149663235-6fsjeg-t500x500.jpg")
+Post.create!(title: "Day 4", description: "Best new song", image_ref: "https://static.stereogum.com/uploads/2018/05/the-1975-give-yourself-a-try-1527789760-640x640.jpg")
 
-Post.create!(title: "Day 5", description: "Great single")
-Post.create!(title: "Day 6", description: "Nice rhyming")
+Post.create!(title: "Day 5", description: "Great single", image_ref: "https://thefader-res.cloudinary.com/private_images/w_760,c_limit,f_auto,q_auto:best/single-artwork-for-frank-ocean-s-lens_xgecql/single-artwork-for-frank-ocean-s-lens.jpg")
+Post.create!(title: "Day 6", description: "Nice rhyming", image_ref: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7c/Frank_Ocean_Chanel_Cover.jpg/220px-Frank_Ocean_Chanel_Cover.jpg")
 
-Track.create!(title: "This Must Be The Place", artist_id: 1, post_id: 1, image_link: "https://www.rhino.com/sites/rhino.com/files/styles/square/public/aod/075992388320.jpg?itok=m1f27u9i")
-Track.create!(title: "Once in a Lifetime", artist_id: 1, post_id: 2, image_link: "https://cdn.albumoftheyear.org/album/remain-in-light.jpg")
+Track.create!(title: "This Must Be The Place", artist_id: Artist.all[0].id, post_id: Post.all[0].id)
+Track.create!(title: "Once in a Lifetime", artist_id: Artist.all[0].id, post_id: Post.all[1].id)
 
-Track.create!(title: "A Change of Heart", artist_id: 2, post_id: 3, image_link: "https://i1.sndcdn.com/artworks-000149663235-6fsjeg-t500x500.jpg")
-Track.create!(title: "Give Yourself a Try", artist_id: 2, post_id: 4, image_link: "https://static.stereogum.com/uploads/2018/05/the-1975-give-yourself-a-try-1527789760-640x640.jpg")
+Track.create!(title: "A Change of Heart", artist_id: Artist.all[1].id, post_id: Post.all[2].id)
+Track.create!(title: "Give Yourself a Try", artist_id: Artist.all[1].id, post_id: Post.all[3].id)
 
-
-Track.create!(title: "Lens", artist_id: 3, post_id: 5, image_link: "https://thefader-res.cloudinary.com/private_images/w_760,c_limit,f_auto,q_auto:best/single-artwork-for-frank-ocean-s-lens_xgecql/single-artwork-for-frank-ocean-s-lens.jpg")
-Track.create!(title: "Chanel", artist_id: 3, post_id: 6, image_link: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7c/Frank_Ocean_Chanel_Cover.jpg/220px-Frank_Ocean_Chanel_Cover.jpg")
+Track.create!(title: "Lens", artist_id: Artist.all[2].id, post_id: Post.all[4].id)
+Track.create!(title: "Chanel", artist_id: Artist.all[2].id, post_id: Post.all[5].id)
 
 
